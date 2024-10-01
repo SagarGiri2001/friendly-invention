a8c86854b6a43166ae13ae470f2f1393f709194d
Sagar Giri <156954707+SagarGiri2001@users.noreply.github.com>
2024-10-01 01:40:30 +0000

    More advanced version of the sum

--- skills.js
+++ skills.js
@@ -1,3 +1,9 @@
-function calcuateNumbers(var1, var2) {
-    console.log(var1 + var2);
+function calculateNumbers(var1, var2) {
+    if (typeof var1 !== 'number' || typeof var2 !== 'number') {
+        console.error('Both arguments must be numbers');
+        return null;
+    }
+    const sum = var1 + var2;
+    console.log(sum);
+    return sum;
 }
